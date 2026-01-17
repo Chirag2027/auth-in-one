2c8b37e90db4e9ce77163fb1aa3ad5f6f84fc9be
Chirag Verma <chirag.verma@zypp.in>
2026-01-18 01:55:53 +0530

    proj setup

---
+++ src/main/java/com/chirag/auth_in_one/auth_app_backend/AuthAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chirag.auth_in_one.auth_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chirag/auth_in_one/auth_app_backend/AuthAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chirag.auth_in_one.auth_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
