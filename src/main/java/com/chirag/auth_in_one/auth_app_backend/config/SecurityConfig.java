de47744b9d07e5b4104393de9e6f867a793a1014
Chirag Verma <chirag.yep@gmail.com>
2026-04-16 10:07:35 +0530

    register login apis bypass auth

--- src/main/java/com/chirag/auth_in_one/auth_app_backend/config/SecurityConfig.java
+++ src/main/java/com/chirag/auth_in_one/auth_app_backend/config/SecurityConfig.java
@@ -2,11 +2,25 @@ package com.chirag.auth_in_one.auth_app_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.requestMatchers("/api/v1/auth/register").permitAll().
+                requestMatchers("/api/v1/auth/login").permitAll().anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
